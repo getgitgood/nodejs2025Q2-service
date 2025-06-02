eecb7d1c76e4ff4bfbd7075a1fe0ba1cbd3bb0b0
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-06-02 17:47:37 +0300

    fix: miss match

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('Home Library Service').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT);
 }
